249122a7a927bbdf9c17d723e9e2f7988d4c92a9
RobLaughlin <waterfallkicks@gmail.com>
2020-09-30 13:46:29 -0700

    Update maze.test.js

--- src/tests/maze.test.js
+++ src/tests/maze.test.js
@@ -3,8 +3,11 @@ import Maze from "../modules/maze/Maze";
 function mazeTest() {
     let maze = new Maze(10, 10);
     let mGen = maze.generator(4, 5);
-    console.log(mGen.next());
-    console.log(mGen.next());
+    let cell;
+    do {
+        cell = mGen.next();
+        console.log(cell);
+    } while (!cell.done);
 }
 
 export default mazeTest;
