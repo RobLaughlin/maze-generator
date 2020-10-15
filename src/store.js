3deddcbd78f576ee677fec245bfcf26ef0994d8a
RobLaughlin <waterfallkicks@gmail.com>
2020-10-15 00:16:04 -0700

    Fix entrance bug

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux';
-import RootReducer from './reducers/Root.reducer';
+import RootReducer from './reducers/root.reducer';
 import DefaultState from './default.state';
 
 const store = createStore(RootReducer, DefaultState);
